d414e77864920f0709076013b06a1f9b389034e2
Oki Husso <oki.husso@gmail.com>
2018-10-20 21:19:14 +0300

    typo in version number 1.1.1 -> 1.1.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         "django >= 1.6",
         "twitter-monitor >= 0.3.0, < 0.4",
-        "swapper >= 1.1.1, < 1.1.2",
+        "swapper >= 1.1.0, < 1.1.2",
         "django-jsonview >= 0.2, < 0.5",
         "django-bootstrap3 >= 4.3.0"
     ],
